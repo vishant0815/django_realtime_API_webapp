a770c5e223094dba0cb4ac3ab7bb91a766a26184
Vishant Bhavsar <vishantbhavsar500@gmail.com>
2022-07-10 20:12:15 +0530

    Update settings.py

--- gtu/settings.py
+++ gtu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j$n7&bsxf2ow9w_mxxioi*0*-c0d0370rq_v^6k7npn1+b#(%j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '']
+ALLOWED_HOSTS = ['127.0.0.1', 'newswebapp1508.herokuapp.com']
 
 
 # Application definition
